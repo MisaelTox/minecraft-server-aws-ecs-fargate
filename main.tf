0b252208f32c9c1e900f3978a42a60fbaa9ecd20
MisaelTox <misaeltoxcatl@gmail.com>
2026-08-07 22:02:46 +0200

    fix: drop redundant CloudWatchLogsFullAccess from task execution role

--- main.tf
+++ main.tf
@@ -151,7 +151,3 @@ resource "aws_iam_role_policy_attachment" "ecs_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_logs" {
-  role       = aws_iam_role.ecs_tasks_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
-}
